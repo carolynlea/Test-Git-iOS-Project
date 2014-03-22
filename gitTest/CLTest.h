304b773ef67bb05126efa7ce91f548255547df23
Carolyn Lea <carolynlea@Adams-iMac.local>
2014-03-22 10:12:23 -0600

    added a test class

---
+++ gitTest/CLTest.h
@@ -0,0 +1,13 @@
+//
+//  CLTest.h
+//  gitTest
+//
+//  Created by Carolyn Lea on 3/22/14.
+//  Copyright (c) 2014 CarolynLea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLTest : NSObject
+
+@end
---
+++ gitTest/CLTest.m
@@ -0,0 +1,13 @@
+//
+//  CLTest.m
+//  gitTest
+//
+//  Created by Carolyn Lea on 3/22/14.
+//  Copyright (c) 2014 CarolynLea. All rights reserved.
+//
+
+#import "CLTest.h"
+
+@implementation CLTest
+
+@end
